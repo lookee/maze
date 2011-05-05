20c4447299eb234588b5df3aba94e0845454eb9e
Luca Amore <luca.amore@gmail.com>
2011-05-05 02:29:09 +0200

    minor comment fix

--- maze.pl
+++ maze.pl
@@ -8,13 +8,13 @@
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
-#    Reversi42 is distributed in the hope that it will be useful,
+#    Maze is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
 #
 #    You should have received a copy of the GNU General Public License
-#    along with Reversi42.  If not, see <http://www.gnu.org/licenses/>.
+#    along with Maze.  If not, see <http://www.gnu.org/licenses/>.
 #------------------------------------------------------------------------
 
 use strict;
